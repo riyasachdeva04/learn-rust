5bb958e511d1477fd3c85a5c11bb562d4155cfa2
riyasachdeva04 <riiya2048@gmail.com>
2024-09-24 01:07:49 +0530

    day 1

---
+++ even/target/debug/a1.d
@@ -0,0 +1,1 @@
+/Users/riyasachdeva/Desktop/rusty/a1/target/debug/a1: /Users/riyasachdeva/Desktop/rusty/a1/src/main.rs
---
+++ even/target/debug/deps/a1-f4584fbb6efcc4c4.d
@@ -0,0 +1,5 @@
+/Users/riyasachdeva/Desktop/rusty/a1/target/debug/deps/a1-f4584fbb6efcc4c4: src/main.rs
+
+/Users/riyasachdeva/Desktop/rusty/a1/target/debug/deps/a1-f4584fbb6efcc4c4.d: src/main.rs
+
+src/main.rs:
